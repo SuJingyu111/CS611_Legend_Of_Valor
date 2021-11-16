dca74eca973cd20e0892a3912f7a82ecb1a8a05d
SuJingyu111 <gar123su@gmail.com>
2021-11-15 23:07:16 -0500

    Adjusted CellFactory.java to be compatible with new Cell types

--- src/CellFactory.java
+++ src/CellFactory.java
@@ -4,17 +4,32 @@ import java.util.Random;
 
 public class CellFactory {
 
-    public Cell getCell() {
+    public Cell getRandomAccessibleCell() {
         Random rand = new Random();
         int randInt = rand.nextInt(10);
         if (randInt < 2) {
-            return new InaccessibleCell();
+            return new Bush();
         }
-        else if (randInt < 5) {
-            return new Market();
+        else if (randInt < 4) {
+            return new Cave();
+        }
+        else if (randInt < 6){
+            return new Koulou();
         }
         else {
-            return new CommonCell();
+            return new PlainCell();
         }
+    }
+
+    public InaccessibleCell getInaccessibleCell() {
+        return new InaccessibleCell();
+    }
+
+    public HeroNexus getHeroNexus() {
+        return new HeroNexus();
+    }
+
+    public MonsterNexus getMonsterNexus() {
+        return new MonsterNexus();
     }
 }
