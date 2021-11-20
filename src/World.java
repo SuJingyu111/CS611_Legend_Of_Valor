f3073307bb552d390c978c427356c169f48365e7
Mdarnell20011 <91031070+Mdarnell20011@users.noreply.github.com>
2021-11-19 19:37:55 -0500

    Update World.java

--- src/World.java
+++ src/World.java
@@ -4,109 +4,39 @@ import java.util.Random;
 
 public class World {
 
-    private Cell[][] map;
-
-    private int heroPosI, heroPosJ;
-
+    private Lane[] map;
+    
     private int dimension;
-
-    private CellFactory cellFactory;
+    
+	private LaneFactory laneFactory;
 
     public World(int dimension) {
         this.dimension = dimension;
-        map = new Cell[dimension][dimension];
-        cellFactory = new CellFactory();
+        map = new Lane[2*dimension+1];
+		laneFactory = new LaneFactory();
+		for (int i=0; i<dimension; i++){
+			map[2*i]=laneFactory.getLane(0);
+			map[2*i+1]=laneFactory.getLane(1);
+		}
+		map[2*dimension]=laneFactory.getLane(0);
     }
 
-    public void placeHeroTeamInit() {
-        Random rand = new Random();
-        heroPosI = rand.nextInt(dimension);
-        heroPosJ = rand.nextInt(dimension);
-        Cell cell = cellFactory.getRandomAccessibleCell();
-        while (cell instanceof InaccessibleCell) {
-            cell = cellFactory.getRandomAccessibleCell();
-        }
-        map[heroPosI][heroPosJ] = cell;
-        initSurrounding();
-    }
+	public void placeHeroInit() {
+		for (int i=0; i<dimension;i++){
+			Hero hero= new Hero();
+			hero.setHeroPosL(i+1);
+			map[2*i+1].placeHeroInit(hero);
+		}
+	}
 
-    public boolean moveHeroTo(int i, int j) {
-        if (heroPosI + i < 0 || heroPosI + i >= dimension || heroPosJ + j < 0 || heroPosJ + j >= dimension || map[heroPosI + i][heroPosJ + j] instanceof InaccessibleCell) {
-            return false;
-        }
-        heroPosI += i;
-        heroPosJ += j;
-        initSurrounding();
-        return true;
-    }
-
-    public void display() {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < dimension; i++) {
-            res.append("+--".repeat(dimension)).append("+\n");
-            for (int j = 0; j < dimension; j++) {
-                res.append('|');
-                if (map[i][j] == null) {
-                    res.append("  ");
-                }
-                else if (map[i][j] instanceof InaccessibleCell) {
-                    res.append("I ");
-                }
-                else if (map[i][j] instanceof Market) {
-                    res.append("M");
-                    if (i == heroPosI && j == heroPosJ) {
-                        res.append("H");
-                    }
-                    else {
-                        res.append(" ");
-                    }
-                }
-                else {
-                    res.append("C");
-                    if (i == heroPosI && j == heroPosJ) {
-                        res.append("H");
-                    }
-                    else {
-                        res.append(" ");
-                    }
-                }
-            }
-            res.append("|\n");
-        }
-        res.append("+--".repeat(dimension)).append("+\n");
-        System.out.println(res);
-    }
-    public Cell getCurCell() {
-        return map[heroPosI][heroPosJ];
-    }
-
-    private void initSurrounding() {
-        boolean blocked = true;
-        int up = Math.max(0, heroPosI - 1), down = Math.min(heroPosI + 1, dimension - 1);
-        int left = Math.max(0, heroPosJ - 1), right = Math.min(heroPosJ + 1, dimension - 1);
-        for (int i = up; i <= down; i++) {
-            for (int j = left; j <= right; j++) {
-                if (!(i == heroPosI && j == heroPosJ) && map[i][j] == null) {
-                    Cell cell = cellFactory.getRandomAccessibleCell();
-                    map[i][j] = cell;
-                    if (! (cell instanceof InaccessibleCell)) {
-                        blocked = false;
-                    }
-                }
-            }
-        }
-        if (blocked) {
-            Random rand = new Random();
-            int unBlockI = rand.nextInt(down - up + 1) + up, unBlockJ = rand.nextInt(right - left + 1) + left;
-            while (unBlockI == heroPosI && unBlockJ == heroPosJ) {
-                unBlockI = rand.nextInt(down - up + 1) + up;
-                unBlockJ = rand.nextInt(right - left + 1) + left;
-            }
-            Cell cell = cellFactory.getRandomAccessibleCell();
-            if (cell instanceof InaccessibleCell) {
-                cell = cellFactory.getRandomAccessibleCell();
-            }
-            map[unBlockI][unBlockJ] = cell;
-        }
-    }
+    public void display(){
+		StringBuilder str= new StringBuilder();
+		for (int i=0; i<this.map[0].getCells().length; i++){
+			for (int j=0; j<this.map.length; j++){
+				str.append(this.map[j].toStringArrayByRow(i)+" ");
+			}
+			str.append("\n");
+		}
+		System.out.println(str);
+	}
 }
